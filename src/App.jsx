0f9260c5c861d72322e5c7eafd5da5d9d981f16c
LuanTang <luantang.work@gmail.com>
2024-07-24 08:55:17 +0700

    change path

--- src/App.jsx
+++ src/App.jsx
@@ -27,16 +27,16 @@ function App() {
       <AuthProvider>
         <FacebookProvider>
           <Routes>
-            <Route exact path="/" element={<Dashboard/>} />
-            <Route exact path="/inspiration/post" element={<Dashboard route="inspiration/post"/>} />
-            <Route exact path="/signin" element={
+            <Route exact path="/nair-web" element={<Dashboard/>} />
+            <Route exact path="/nair-web/inspiration/post" element={<Dashboard route="inspiration/post"/>} />
+            <Route exact path="/nair-web/signin" element={
               <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-md w-full space-y-8">
                   <LoginPage />
                 </div>
               </div>
             } />
-            <Route exact path="/signup" element={
+            <Route exact path="/nair-web/signup" element={
               <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-md w-full space-y-8">
                   <SignupPage />
@@ -51,4 +51,3 @@ function App() {
 }
 
 export default App;
-  
